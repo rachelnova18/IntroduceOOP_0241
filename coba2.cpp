ff3e5f96699ac33f90cf924664104ffb48048adb
rachelnova18 <rachelnv26@gmail.com>
2025-04-28 19:21:33 +0700

    membuat class persegi panjang

--- coba2.cpp
+++ coba2.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class persegipanjang
+{
+public:
+    float panjang;
+    float lebar;
+    float luas;
+
+    void inputData()
+    {
+        cout << " input panjang : ";
+        cin >> panjang;
+        cout << " input lebar : ";
+        cin >> lebar;
+    }
+
+    void menghitungLuas()
+    {
+        luas = panjang * lebar;
+    }
+
+    void output()
+    {
+        cout << " luas persegi panjang = " << luas << endl;
+    }
+};
+
