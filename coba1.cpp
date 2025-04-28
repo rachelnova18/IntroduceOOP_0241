b1556c542e1232f746cc9ddb5655969032eaf008
rachelnova18 <rachelnv26@gmail.com>
2025-04-28 19:18:38 +0700

    membuat class nama planet dengan atribut nama, diameter, gravitasi, displayData, dan inputData

--- coba1.cpp
+++ coba1.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class planet
+{
+private:
+    float gravitasi;
+
+public:
+    string nama;
+    float diameter;
+
+    void displaydata()
+    {
+        cout << ", gravitasiku " << gravitasi << endl;
+    }
+    void inputdata()
+    {
+        cout << "input nama : ";
+        cin >> nama;
+        cout << "ukuranku : ";
+        cin >> diameter;
+        cout << "gravitasiku :";
+        cin >> gravitasi;
+    }
+};
